5140e9658d375b05ee28ef726d7bca423f949a46
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2016-12-23 18:01:43 -0500

    Don't print vault version in success case

--- 0.6.4/docker-entrypoint.sh
+++ 0.6.4/docker-entrypoint.sh
@@ -82,7 +82,7 @@ if [ "$1" = 'vault' ]; then
     setcap cap_ipc_lock=+ep $(readlink -f $(which vault))
 
     # In the case vault has been started in a container without IPC_LOCK privileges
-    if ! vault -version 2> /dev/null; then
+    if ! vault -version 1>/dev/null 2>/dev/null; then
         >&2 echo "Couldn't start vault with IPC_LOCK. Disabling IPC_LOCK, please use --privileged or --cap-add IPC_LOCK"
         setcap -r $(readlink -f $(which vault))
     fi
