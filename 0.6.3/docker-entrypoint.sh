e8edfef53deb64e1898fea66e5fb7488b7dc3154
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2016-12-31 11:27:46 -0500

    Add SKIP_SETCAP to skip setcap logic entirely

--- 0.6.3/docker-entrypoint.sh
+++ 0.6.3/docker-entrypoint.sh
@@ -78,13 +78,15 @@ if [ "$1" = 'vault' ]; then
         chown -R vault:vault /vault/file
     fi
 
-    # Allow mlock to avoid swapping Vault memory to disk
-    setcap cap_ipc_lock=+ep $(readlink -f $(which vault))
+    if [ -z "$SKIP_SETCAP" ]; then
+        # Allow mlock to avoid swapping Vault memory to disk
+        setcap cap_ipc_lock=+ep $(readlink -f $(which vault))
 
-    # In the case vault has been started in a container without IPC_LOCK privileges
-    if ! vault -version 1>/dev/null 2>/dev/null; then
-        >&2 echo "Couldn't start vault with IPC_LOCK. Disabling IPC_LOCK, please use --privileged or --cap-add IPC_LOCK"
-        setcap -r $(readlink -f $(which vault))
+        # In the case vault has been started in a container without IPC_LOCK privileges
+        if ! vault -version 1>/dev/null 2>/dev/null; then
+            >&2 echo "Couldn't start vault with IPC_LOCK. Disabling IPC_LOCK, please use --privileged or --cap-add IPC_LOCK"
+            setcap -r $(readlink -f $(which vault))
+        fi
     fi
 
     set -- gosu vault "$@"
