50742dac562279e96e67be5b24946359a0ebdc05
claire-labry <claire@hashicorp.com>
2021-04-01 11:38:42 -0500

    updated image version + cleaning up some comments

--- scripts/tag-images.sh
+++ scripts/tag-images.sh
@@ -15,7 +15,7 @@ function get_latest_docker_version {
    #   0 - success (version in the 'latest' container echoed)
    #   1 - 'latest' tag does not exist or label could not be found
 
-   docker pull "$1"/"$2":latest 
+   docker pull "$1"/"$2":latest
    local docker_latest=$(docker inspect --format="{{ index .Config.Labels.version }}" "$1"/"$2":latest)
    if [ -z "$docker_latest" ]; then
       return 1
@@ -41,31 +41,31 @@ function higher_version {
 # If this is not a pre-release, we determine if any of the 'latest' images need to be updated
 function main() {
 
-      : "${VERSION?"Need to set VERSION"}"
-      
-      : "${REGISTRY_NAME?"Need to set REGISTRY_NAME"}"
-      
-      : "${PROJECT_NAME?"Need to set PROJECT_NAME"}
+	: "${VERSION?"Need to set VERSION"}"
 
-   #  LATEST_DOCKER_VERSION=$(get_latest_docker_version "$DOCKER_ORG" "$PROJECT_NAME")
-      # echo "latest docker version is: $LATEST_DOCKER_VERSION"
+	: "${REGISTRY_NAME?"Need to set REGISTRY_NAME"}"
 
-    DOCKER_TAG="$VERSION$TAG_SUFFIX"
-   
+	: "${PROJECT_NAME?"Need to set PROJECT_NAME"}"
+
+	# LATEST_DOCKER_VERSION=$(get_latest_docker_version "$REGISTRY_NAME" "$PROJECT_NAME") || return 1
+	# echo "latest docker version is: $LATEST_DOCKER_VERSION"
+
+	DOCKER_TAG="$VERSION$TAG_SUFFIX"
+
    if [[ "${VERSION}" =~ [0-9]+\.[0-9]+\.[0-9]+$ ]]; then
       # check to see if the current tag is higher than the latest version on dockerhub
-      #  HIGHER_LATEST_VERSION=$(higher_version "$DOCKER_TAG" "$LATEST_DOCKER_VERSION")
-      #  echo "Docker tag is: $DOCKER_TAG"
-      #  echo "Latest tag is: $LATEST_DOCKER_VERSION"
-      #  echo "Higher version is: $HIGHER_LATEST_VERSION"
+    #   HIGHER_LATEST_VERSION=$(higher_version "$VERSION" "$LATEST_DOCKER_VERSION")
+    #   echo "Image version is: $VERSION"
+    #   echo "Latest tag is: $LATEST_DOCKER_VERSION"
+    #   echo "Higher version is: $HIGHER_LATEST_VERSION"
       # if:
       #   * we didn't find a version from the 'latest' image, it means it doesn't exist
       #   * or if the current tag version is higher than the latest docker one (or the same)
       # we build latest
-      # if [ -z "$LATEST_DOCKER_VERSION" ] || [ "$HIGHER_LATEST_VERSION" = "$VERSION" ]; then
+    #   if [ -z "$LATEST_DOCKER_VERSION" ] || [ "$HIGHER_LATEST_VERSION" = "$VERSION" ]; then
          echo "Tagging a new latest docker image"
          docker tag "$REGISTRY_NAME"/"$PROJECT_NAME":"$DOCKER_TAG" "$REGISTRY_NAME"/"$PROJECT_NAME":latest || return 1
-      # fi
+    #   fi
    fi
    # If it is a prerelease, we don't tag latest so we do nothing here
 
