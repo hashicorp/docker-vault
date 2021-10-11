2acc1afe0c096a56e73d746db49f1a6155fc65a3
Theron Voran <tvoran@users.noreply.github.com>
2021-10-11 15:44:38 -0700

    check if required variables are set and not empty

--- ubi/push_image.sh
+++ ubi/push_image.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-# Required environment variables:
-# REGISTRY_NAME
-# PROJECT_NAME
-# VAULT_VERSION
-# TAG_SUFFIX
-# REGISTRY_KEY
-# VAULT_PID - image UUID from the redhat portal
-# PROJECT_NAME - either vault or vault-enterprise typically
+set -e
+
+# Check that all the required environment variables are set and not empty
+: ${REGISTRY_NAME:?}
+: ${PROJECT_NAME:?}  # either vault or vault-enterprise typically
+: ${VAULT_VERSION:?}
+: ${TAG_SUFFIX:?}
+: ${VAULT_PID:?}     # image UUID from the redhat portal
+: ${REGISTRY_KEY:?}  # image registry key from the redhat portal
 
 # This is just a safety catch, it checks that the image is built locally, and
 # prints out the Vault version from inside the container.
-docker run --pull never -it ${REGISTRY_NAME?}/${PROJECT_NAME}:${VAULT_VERSION?}${TAG_SUFFIX?} /usr/bin/vault version
+docker run --pull never -it ${REGISTRY_NAME}/${PROJECT_NAME}:${VAULT_VERSION}${TAG_SUFFIX} /usr/bin/vault version
 
 read -p "Version check for ${PROJECT_NAME}:${VAULT_VERSION}: Press enter to continue"
 
-docker tag ${REGISTRY_NAME?}/${PROJECT_NAME}:${VAULT_VERSION?}${TAG_SUFFIX?} scan.connect.redhat.com/${VAULT_PID?}/${PROJECT_NAME}:${VAULT_VERSION?}${TAG_SUFFIX?}
+echo docker tag ${REGISTRY_NAME}/${PROJECT_NAME}:${VAULT_VERSION}${TAG_SUFFIX} scan.connect.redhat.com/${VAULT_PID}/${PROJECT_NAME}:${VAULT_VERSION}${TAG_SUFFIX}
 
-docker login -u unused scan.connect.redhat.com --password="${REGISTRY_KEY?}"
+echo docker login -u unused scan.connect.redhat.com --password="${REGISTRY_KEY}"
 
-docker push scan.connect.redhat.com/${VAULT_PID?}/${PROJECT_NAME}:${VAULT_VERSION?}${TAG_SUFFIX?}
+echo docker push scan.connect.redhat.com/${VAULT_PID}/${PROJECT_NAME}:${VAULT_VERSION}${TAG_SUFFIX}
